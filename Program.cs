a7d0bf589c145dee87c30330e4c00957abe37316
aftabpatel10 <aftabpatel3107@gmail.com>
2022-11-12 14:29:09 +0530

    Snakeladderprogramme

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          int position = 0;
+            Console.WriteLine($"Game at initial position : {position}");
+         
+            
+            Console.ReadLine();
+        }
+    }
+}
