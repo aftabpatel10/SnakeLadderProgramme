8c7e6a082f2561e9133b84752f0a0a1ce605b4e7
aftabpatel10 <aftabpatel3107@gmail.com>
2022-11-14 22:15:50 +0530

    Cheking Winning till 100th Position

--- Program.cs
+++ Program.cs
@@ -10,30 +10,47 @@ namespace SnakeLadder
     {
         static void Main(string[] args)
         {
-          int position = 0;
+            Console.WriteLine("Welcome To Snake Ladder Programme");
+            int position = 0;
             Console.WriteLine($"Game at initial position : {position}");
 
-            Random random = new Random();
-            int diceoutput = random.Next(1, 7);
+           
             {
-                Random random2 = new Random();
-                int dicecount = random.Next(1, 4);
-                switch(dicecount)
+
+                while (position >= 0 && position < 100)
                 {
-                    case 1:
-                        Console.WriteLine("Ladder");
-                        break;
-                    case 2:
-                        Console.WriteLine("Snake");
-                        break ;
-                    default:
-                        Console.WriteLine("No Play");
-                        break;                       
+                    Random random = new Random();
+                    int diceoutput = random.Next(1, 7);
+                    Console.WriteLine("Dice Output is ;" + diceoutput);
+                    position = position + diceoutput;
+                    Console.WriteLine("Position :" + position);
+
+                    Random random2 = new Random();
+                    Console.WriteLine("Checking For Options");
+                    int dicecount = random.Next(1, 4);
+                    switch (dicecount)
+                    {
+                        case 1:
+                            Console.WriteLine("Ladder");
+                            position += position;
+                            break;
+                        case 2:
+                            Console.WriteLine("Snake");
+                            position -= position;
+                            break;
+                        default:
+                            Console.WriteLine("No Play");
+                            position = position;
+                            break;
+                    }
+                    Console.WriteLine($"rollingdice : {dicecount}");
+                    Console.ReadLine();
                 }
+
+                
             }
 
-            Console.WriteLine($"rollingdice :{diceoutput}");
-            Console.ReadLine();
+            
         }
     }
 }
