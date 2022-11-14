32419686a81aa3ba881356313334df2ab7ade718
aftabpatel10 <aftabpatel3107@gmail.com>
2022-11-14 21:23:20 +0530

    Checking No Play

--- Program.cs
+++ Program.cs
@@ -15,6 +15,23 @@ namespace SnakeLadder
 
             Random random = new Random();
             int diceoutput = random.Next(1, 7);
+            {
+                Random random2 = new Random();
+                int dicecount = random.Next(1, 4);
+                switch(dicecount)
+                {
+                    case 1:
+                        Console.WriteLine("Ladder");
+                        break;
+                    case 2:
+                        Console.WriteLine("Snake");
+                        break ;
+                    default:
+                        Console.WriteLine("No Play");
+                        break;                       
+                }
+            }
+
             Console.WriteLine($"rollingdice :{diceoutput}");
             Console.ReadLine();
         }
